ce272935a0120f56a9e758164d1c7e574cb746fd
Lucas Gomes Matos <lgomes.lg99@gmail.com>
2022-08-20 16:59:36 -0300

    Foto Post

--- src/Hooks/useForm.jsx
+++ src/Hooks/useForm.jsx
@@ -11,6 +11,10 @@ const types = {
     message:
       'Senha precisa ter 1 caractere maiúsculo, 1 minúsculo e 1 digito. Com no mínimo 8 caracteres',
   },
+  number: {
+    regex: /^\d+$/,
+    message: 'Utilize números apenas',
+  },
 };
 
 const useForm = (type) => {
--- src/Pages/User/User.jsx
+++ src/Pages/User/User.jsx
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router-dom';
 import Feed from '../../Components/Feed/Feed';
 import UserHeader from './UserHeader';
 import UserPhotoPost from './UserPhotoPost';
+import UserStats from './UserStats';
 
 export const User = () => {
   return (
@@ -11,7 +12,7 @@ export const User = () => {
       <Routes>
         <Route path="/" element={<Feed />} />
         <Route path="postar" element={<UserPhotoPost />} />
-        <Route path="estatisticas" element={<UserPhotoPost />} />
+        <Route path="estatisticas" element={<UserStats />} />
       </Routes>
     </section>
   );
