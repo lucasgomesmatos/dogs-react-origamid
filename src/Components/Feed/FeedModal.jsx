9c6ff40a3dab73a4f8fec34f4ed4cf37fa318ec4
Lucas Gomes Matos <lgomes.lg99@gmail.com>
2022-08-23 10:59:19 -0300

    feed modal

--- src/Components/Feed/FeedModal.jsx
+++ src/Components/Feed/FeedModal.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { PHOTO_GET } from '../../api';
 import useFetch from '../../Hooks/useFetch';
 import styles from './FeedModal.module.css';
@@ -6,18 +6,22 @@ import Error from '../../Helper/Error';
 import Loading from '../../Helper/Loading';
 import { PhotoContent } from '../Photo/PhotoContent';
 
-const FeedModal = ({ photo }) => {
+const FeedModal = ({ photo, setModalPhoto }) => {
   const { data, error, loading, request } = useFetch();
-  console.log('oi');
 
-  useFetch(() => {
+  useEffect(() => {
     const { url, options } = PHOTO_GET(photo.id);
-    console.log(url);
     request(url, options);
   }, [photo, request]);
 
+  function handleOutsideClick(event) {
+    if (event.target === event.currentTarget) {
+      setModalPhoto(null);
+    }
+  }
+
   return (
-    <div className={styles.modal}>
+    <div className={styles.modal} onClick={handleOutsideClick}>
       {error && <Error error={error} />}
       {loading && <Loading />}
       {data && <PhotoContent data={data} />}
--- src/Components/Photo/PhotoContent.jsx
+++ src/Components/Photo/PhotoContent.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import styles from './PhotoContent.module.css';
 import { Link } from 'react-router-dom';
+import PhotoComments from './PhotoComments';
 
 export const PhotoContent = ({ data }) => {
-  console.log(data);
   const { photo, comments } = data;
   return (
     <div className={styles.photo}>
@@ -12,11 +12,22 @@ export const PhotoContent = ({ data }) => {
       </div>
       <div className={styles.details}>
         <div>
-          <p>
+          <p className={styles.author}>
             <Link to={`/perfil/${photo.author}`}>@{photo.author}</Link>
+            <span className={styles.visualization}>{photo.acessos}</span>
           </p>
+          <h1 className="title">
+            <Link to={`/foto/${photo.id}`}>{photo.title}</Link>
+          </h1>
+          <ul className={styles.attributes}>
+            <li>{photo.peso} kg</li>
+            <li>
+              {photo.idade === 1 ? `${photo.idade} ano` : `${photo.idade} anos`}
+            </li>
+          </ul>
         </div>
       </div>
+      <PhotoComments id={photo.id} comments={comments} />
     </div>
   );
 };
