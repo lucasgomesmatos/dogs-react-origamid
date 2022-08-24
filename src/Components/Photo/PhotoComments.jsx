239628757c3ada08ce5dcc40ee4ec1eebd5ca286
Lucas Gomes Matos <lgomes.lg99@gmail.com>
2022-08-23 23:41:24 -0300

    photo comment

--- src/Components/Photo/PhotoComments.jsx
+++ src/Components/Photo/PhotoComments.jsx
@@ -1,7 +1,25 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
+import { UserContext } from '../../UserContext';
+import PhotoCommentsForm from './PhotoCommentsForm';
+import styles from './PhotoComments.module.css';
 
-const PhotoComments = () => {
-  return <div>PhotoComments</div>;
+const PhotoComments = (pros) => {
+  const [comments, setComments] = useState(() => pros.comments);
+  const { login } = useContext(UserContext);
+
+  return (
+    <>
+      <ul className={styles.comments}>
+        {comments.map((comment) => (
+          <li key={comment.comment_ID}>
+            <strong>{comment.comment_author}:</strong>
+            <span>{comment.comment_content}</span>
+          </li>
+        ))}
+      </ul>
+      {login && <PhotoCommentsForm id={pros.id} setComments={setComments} />}
+    </>
+  );
 };
 
 export default PhotoComments;
