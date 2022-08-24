f8f9073ee232681716c148070225bade56ff4869
Lucas Gomes Matos <lgomes.lg99@gmail.com>
2022-08-24 08:44:04 -0300

    form comments

--- src/Components/Photo/PhotoComments.jsx
+++ src/Components/Photo/PhotoComments.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useEffect, useRef, useState } from 'react';
 import { UserContext } from '../../UserContext';
 import PhotoCommentsForm from './PhotoCommentsForm';
 import styles from './PhotoComments.module.css';
@@ -6,10 +6,15 @@ import styles from './PhotoComments.module.css';
 const PhotoComments = (pros) => {
   const [comments, setComments] = useState(() => pros.comments);
   const { login } = useContext(UserContext);
+  const commentsSection = useRef(null);
+
+  useEffect(() => {
+    commentsSection.current.scrollTop = commentsSection.current.scrollHeight;
+  }, [comments]);
 
   return (
     <>
-      <ul className={styles.comments}>
+      <ul ref={commentsSection} className={styles.comments}>
         {comments.map((comment) => (
           <li key={comment.comment_ID}>
             <strong>{comment.comment_author}:</strong>
