8c9e14b191398821b1b97871b2ef728e50ffda85
Lucas Gomes Matos <lgomes.lg99@gmail.com>
2022-08-29 23:28:32 -0300

    assets path

--- src/Pages/User/UserHeaderNav.jsx
+++ src/Pages/User/UserHeaderNav.jsx
@@ -1,10 +1,10 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { NavLink, useLocation } from 'react-router-dom';
 import { UserContext } from '../../UserContext';
-import { ReactComponent as MinhasFotos } from '../../Assets/feed.svg';
-import { ReactComponent as Estatisticas } from '../../Assets/estatisticas.svg';
-import { ReactComponent as AdicionarFoto } from '../../Assets/adicionar.svg';
-import { ReactComponent as Sair } from '../../Assets/sair.svg';
+import { ReactComponent as MinhasFotos } from '../../assets/feed.svg';
+import { ReactComponent as Estatisticas } from '../../assets/estatisticas.svg';
+import { ReactComponent as AdicionarFoto } from '../../assets/adicionar.svg';
+import { ReactComponent as Sair } from '../../assets/sair.svg';
 import styles from './UserHeaderNav.module.css';
 import useMedia from '../../Hooks/useMedia';
 
